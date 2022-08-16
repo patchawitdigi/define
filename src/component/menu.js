e58d9015f62d0930b4185f4398ccca3d84d54ea5
patchawitdigi <110367084+patchawitdigi@users.noreply.github.com>
2022-08-16 16:20:14 +0700

    menu

---
+++ src/component/menu.js
@@ -0,0 +1,23 @@
+import "./menu.css";
+
+export default function Menu() {
+    return (
+        <div >
+            <div className="menuclick">
+
+            </div>
+            <div className="logodef">
+                <img />
+            </div>
+            <div>
+                <p>OVERVIEW</p>
+                <p>POINT OF INTERESTS</p>
+                <p>HIDDEN TREASURES</p>
+                <p>LUXURY OF CHOICES</p>
+                <p>GALLERIE D’ARTE</p>
+                <p>NEWS & MEDIA</p>
+            </div>
+        </div>
+
+    )
+}
