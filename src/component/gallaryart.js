1d7baee1bbfd0571f793188abe50c6dfdad1b2a4
Patchawit Muensoontorn <patchawit@digitiveworks.com>
2022-08-19 12:14:31 +0700

    ui

--- src/component/gallaryart.js
+++ src/component/gallaryart.js
@@ -3,60 +3,208 @@ import delogo from '../img/pointint/logo-menu.png'
 import Facade1 from '../img/gallaryart/facade1.jpeg'
 import Facade2 from '../img/gallaryart/facade2.jpeg'
 import Facade3 from '../img/gallaryart/facade3.jpeg'
+
 import Outdoor1 from '../img/gallaryart/outdoor1.jpeg'
 import Outdoor2 from '../img/gallaryart/outdoor2.jpeg'
 import Outdoor3 from '../img/gallaryart/outdoor3.jpeg'
+import Outdoor4 from '../img/overview/gallery-4.jpeg'
+import Outdoor5 from '../img/hidden/Artwork.jpg'
+import Outdoor6 from '../img/hidden/Swing.jpg'
+import Outdoor7 from '../img/hidden/Sunbed.jpg'
+import Outdoor8 from '../img/hidden/Gate.jpg'
+import Outdoor9 from '../img/hidden/TheArt.jpg'
+
 import Indoor1 from '../img/gallaryart/indoor1.jpeg'
 import Indoor2 from '../img/gallaryart/indoor2.jpeg'
 import Indoor3 from '../img/gallaryart/indoor3.jpeg'
+import Indoor4 from '../img/hidden/Parking.jpg'
+import Indoor5 from '../img/hidden/Lobby.jpg'
+import Indoor6 from '../img/hidden/Lift.jpg'
+import Indoor7 from '../img/hidden/working.jpg'
+import Indoor8 from '../img/hidden/Kid.jpg'
+
 import Riviere1 from '../img/gallaryart/riviere1.jpeg'
 import Riviere2 from '../img/gallaryart/riviere2.jpeg'
 import Riviere3 from '../img/gallaryart/riviere3.jpeg'
+import Riviere4 from '../img/gallaryart/riviere4.jpg'
+import Riviere5 from '../img/gallaryart/riviere5.jpg'
+
 import Jardin1 from '../img/gallaryart/jardin1.jpeg'
 import Jardin2 from '../img/gallaryart/jardin2.jpeg'
 import Jardin3 from '../img/gallaryart/jardin3.jpeg'
+import Jardin4 from '../img/gallaryart/jardin4.jpg'
+import Jardin5 from '../img/gallaryart/jardin5.jpg'
+import Jardin6 from '../img/gallaryart/jardin6.jpg'
+
 import { BsPlusCircle } from "react-icons/bs"
 import { FiArrowRight } from "react-icons/fi";
 import { FiArrowLeft } from "react-icons/fi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Gallaryart() {
 
-  
-
     useEffect(() => {
-        var modal = document.getElementById("myModal");
-        var btn = document.getElementById("plusitem1");
-        var span = document.getElementsByClassName("close")[0];
-        btn.onclick = function () {
-            modal.style.display = "block";
+        var modal01 = document.getElementById("myModal01");
+        var btn01 = document.getElementById("plusitem1");
+        var span01 = document.getElementsByClassName("close")[0];
+        btn01.onclick = function () {
+            modal01.style.display = "block";
         }
-        span.onclick = function () {
-            modal.style.display = "none";
+        span01.onclick = function () {
+            modal01.style.display = "none";
+        }
+
+        var modal02 = document.getElementById("myModal02");
+        var btn02 = document.getElementById("plusitem2");
+        var span02 = document.getElementsByClassName("close")[1];
+        btn02.onclick = function () {
+            modal02.style.display = "block";
+        }
+        span02.onclick = function () {
+            modal02.style.display = "none";
+        }
+
+        var modal03 = document.getElementById("myModal03");
+        var btn03 = document.getElementById("plusitem3");
+        var span03 = document.getElementsByClassName("close")[2];
+        btn03.onclick = function () {
+            modal03.style.display = "block";
+        }
+        span03.onclick = function () {
+            modal03.style.display = "none";
+        }
+        
+        var modal04 = document.getElementById("myModal04");
+        var btn04 = document.getElementById("plusitem4");
+        var span04 = document.getElementsByClassName("close")[3];
+        btn04.onclick = function () {
+            modal04.style.display = "block";
+        }
+        span04.onclick = function () {
+            modal04.style.display = "none";
+        }
+        
+        var modal05 = document.getElementById("myModal05");
+        var btn05 = document.getElementById("plusitem5");
+        var span05 = document.getElementsByClassName("close")[4];
+        btn05.onclick = function () {
+            modal05.style.display = "block";
+        }
+        span05.onclick = function () {
+            modal05.style.display = "none";
         }
 
     }, [])
 
-    // var slideIndex = 1;
-    // showDivs(slideIndex);
+    const [imgsl01, setimgsl01] = useState(0)
+    const slide01 = [Facade1, Facade2, Facade3]
+    function next01(){
+        if(imgsl01 >= slide01.length-1){
+            setimgsl01(0)
+        }
+        else{
+            setimgsl01(imgsl01+1)
+        }
+        console.log(imgsl01)
+    }
+    function prev01(){
+        if(imgsl01 <= 0){
+            setimgsl01(slide01.length - 1)
+        }
+        else{
+            setimgsl01(imgsl01-1)
+        }
 
-    // function plusDivs() {
-    //     showDivs(slideIndex += 1);
-    // }
-    // function minusDivs() {
-    //     showDivs(slideIndex += -1);
-    // }
+        console.log(imgsl01)
+    }
 
-    // function showDivs(n) {
-    //     var i;
-    //     var x = document.getElementsByClassName("mySlides");
-    //     if (n > x.length) { slideIndex = 1 }
-    //     if (n < 1) { slideIndex = x.length }
-    //     for (i = 0; i < x.length; i++) {
-    //         x[i].style.display = "none";
-    //     }
-    //     x[slideIndex - 1].style.display = "block";
-    // }
+    const [imgsl02, setimgsl02] = useState(0)
+    const slide02 = [Outdoor4, Outdoor5, Outdoor6, Outdoor2, Outdoor3, Outdoor1, Outdoor7, Outdoor8, Outdoor9]
+    function next02(){
+        if(imgsl02 >= slide02.length-1){
+            setimgsl02(0)
+        }
+        else{
+            setimgsl02(imgsl02+1)
+        }
+        console.log(imgsl02)
+    }
+    function prev02(){
+        if(imgsl02 <= 0){
+            setimgsl02(slide02.length - 1)
+        }
+        else{
+            setimgsl02(imgsl02-1)
+        }
+
+        console.log(imgsl02)
+    }
+
+    const [imgsl03, setimgsl03] = useState(0)
+    const slide03 = [Indoor4, Indoor5, Indoor6, Indoor3, Indoor1, Indoor2, Indoor7, Indoor8]
+    function next03(){
+        if(imgsl03 >= slide03.length-1){
+            setimgsl03(0)
+        }
+        else{
+            setimgsl03(imgsl03+1)
+        }
+        console.log(imgsl03)
+    }
+    function prev03(){
+        if(imgsl03 <= 0){
+            setimgsl03(slide03.length - 1)
+        }
+        else{
+            setimgsl03(imgsl03-1)
+        }
+
+        console.log(imgsl03)
+    }
+    
+    const [imgsl04, setimgsl04] = useState(0)
+    const slide04 = [Riviere4, Riviere2, Riviere1, Riviere3, Riviere5]
+    function next04(){
+        if(imgsl04 >= slide04.length-1){
+            setimgsl04(0)
+        }
+        else{
+            setimgsl04(imgsl04+1)
+        }
+        console.log(imgsl04)
+    }
+    function prev04(){
+        if(imgsl04 <= 0){
+            setimgsl04(slide04.length - 1)
+        }
+        else{
+            setimgsl04(imgsl04-1)
+        }
+
+        console.log(imgsl04)
+    }
+
+    const [imgsl05, setimgsl05] = useState(0)
+    const slide05 = [Jardin1, Jardin2, Jardin4, Jardin5, Jardin6, Jardin3]
+    function next05(){
+        if(imgsl05 >= slide05.length-1){
+            setimgsl05(0)
+        }
+        else{
+            setimgsl05(imgsl05+1)
+        }
+        console.log(imgsl05)
+    }
+    function prev05(){
+        if(imgsl05 <= 0){
+            setimgsl05(slide05.length - 1)
+        }
+        else{
+            setimgsl05(imgsl05-1)
+        }
+
+        console.log(imgsl05)
+    }
 
 
     return (
@@ -74,27 +222,25 @@ export default function Gallaryart() {
                 <div className="rgallary">
                     <div className="moreitem">
                         <p>FACADE</p>
-                        <BsPlusCircle size="2.5vw" id='plusitem1' />
+                        <BsPlusCircle size="2vw" id='plusitem1' />
                     </div>
                     <img className="facade3 bwimg" src={Facade3} />
                 </div>
             </div>
 
-            <div id="myModal" class="modal2">
+            <div id="myModal01" class="modal2">
                 <span className="close">&times;</span>
                 <div className="modal2-content">
-                    <img src={Facade1} className="mySlides" />
-                    <img src={Facade2} className="mySlides" />
-                    <img src={Facade3} className="mySlides" />
+                    <img src={slide01[imgsl01]} />
                 </div>
-                <FiArrowLeft  className="prev" size="3vw" />
-                <FiArrowRight  className="next" size="3vw" />
+                <FiArrowLeft onClick={prev01} className="prev" size="3vw" />
+                <FiArrowRight onClick={next01} className="next" size="3vw" />
             </div>
 
             <div className="gallary">
                 <div className="rgallary2">
                     <div className="moreitem">
-                        <BsPlusCircle size="2.5vw" id='plusitem2' />
+                        <BsPlusCircle size="2vw" id='plusitem2' />
                         <p>OUTDOOR<br />FACILITIES</p>
                     </div>
                     <img className="outdoor3 bwimg" src={Outdoor3} />
@@ -105,6 +251,14 @@ export default function Gallaryart() {
                 </div>
             </div>
 
+            <div id="myModal02" class="modal2">
+                <span className="close">&times;</span>
+                <div className="modal2-content">
+                    <img src={slide02[imgsl02]} />
+                </div>
+                <FiArrowLeft onClick={prev02} className="prev" size="3vw" />
+                <FiArrowRight onClick={next02} className="next" size="3vw" />
+            </div>
 
             <div className="gallary">
                 <div className="lgallary">
@@ -114,17 +268,25 @@ export default function Gallaryart() {
                 <div className="rgallary">
                     <div className="moreitem">
                         <p>INDOOR<br />FACILITIES</p>
-                        <BsPlusCircle size="2.5vw" id='plusitem3' />
+                        <BsPlusCircle size="2vw" id='plusitem3' />
                     </div>
                     <img className="facade3 bwimg" src={Indoor3} />
                 </div>
             </div>
 
+            <div id="myModal03" class="modal2">
+                <span className="close">&times;</span>
+                <div className="modal2-content">
+                    <img src={slide03[imgsl03]} />
+                </div>
+                <FiArrowLeft onClick={prev03} className="prev" size="3vw" />
+                <FiArrowRight onClick={next03} className="next" size="3vw" />
+            </div>
 
             <div className="gallary">
                 <div className="rgallary2 rgallary3">
                     <div className="moreitem">
-                        <BsPlusCircle size="2.5vw" id='plusitem4' />
+                        <BsPlusCircle size="2vw" id='plusitem4' />
                         <p>RIVIERE</p>
                     </div>
                     <img className="Riviere3 bwimg" src={Riviere3} />
@@ -135,6 +297,14 @@ export default function Gallaryart() {
                 </div>
             </div>
 
+            <div id="myModal04" class="modal2">
+                <span className="close">&times;</span>
+                <div className="modal2-content">
+                    <img src={slide04[imgsl04]} />
+                </div>
+                <FiArrowLeft onClick={prev04} className="prev" size="3vw" />
+                <FiArrowRight onClick={next04} className="next" size="3vw" />
+            </div>
 
             <div className="gallary">
                 <div className="lgallary">
@@ -144,10 +314,19 @@ export default function Gallaryart() {
                 <div className="rgallary">
                     <div className="moreitem">
                         <p>JARDIN</p>
-                        <BsPlusCircle size="2.5vw" id='plusitem5' />
+                        <BsPlusCircle size="2vw" id='plusitem5' />
                     </div>
                     <img className="jardin3 bwimg" src={Jardin3} />
                 </div>
+            </div>
+
+            <div id="myModal05" class="modal2">
+                <span className="close">&times;</span>
+                <div className="modal2-content">
+                    <img src={slide05[imgsl05]} />
+                </div>
+                <FiArrowLeft onClick={prev05} className="prev" size="3vw" />
+                <FiArrowRight onClick={next05} className="next" size="3vw" />
             </div>
 
 
--- src/component/slide.js
+++ src/component/slide.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import "./slide.css";
 import delogo from '../img/pointint/logo-menu.png'
 import bgleft from '../img/overview/bgleft.jpg'
 import im1 from '../img/overview/gallery-4.jpeg'
-import im2 from '../img/hidden/Artwork.jpg'
-import im3 from '../img/hidden/Pool.jpg'
-import im4 from '../img/hidden/Charging.jpg'
-import im5 from '../img/hidden/Gate.jpg'
-import im6 from '../img/hidden/Sunbed.jpg'
-import im7 from '../img/hidden/Swing.jpg'
-import im8 from '../img/hidden/TheArt.jpg'
-import im9 from '../img/hidden/Walkway.jpg'
-import im10 from '../img/hidden/Parking.jpg'
-import im11 from '../img/hidden/Lobby.jpg'
-import im12 from '../img/hidden/Kid.jpg'
+import im2 from '../img/hidden/Artwork2.jpg'
+import im3 from '../img/hidden/Pool2.jpg'
+import im4 from '../img/hidden/Charging2.jpg'
+import im5 from '../img/hidden/Gate2.jpg'
+import im6 from '../img/hidden/Sunbed2.jpg'
+import im7 from '../img/hidden/Swing2.jpg'
+import im8 from '../img/hidden/TheArt2.jpg'
+import im9 from '../img/hidden/Walkway2.jpg'
+import im10 from '../img/hidden/Parking2.jpg'
+import im11 from '../img/hidden/Lobby2.jpg'
+import im12 from '../img/hidden/Kid2.jpg'
 import im13 from '../img/hidden/Lounge.jpg'
-import im14 from '../img/hidden/working.jpg'
-import im15 from '../img/hidden/Lift.jpg'
+import im14 from '../img/hidden/working2.jpg'
+import im15 from '../img/hidden/Lift2.jpg'
 import im16 from '../img/hidden/Party.jpg'
-import im17 from '../img/hidden/Garage.jpg'
+import im17 from '../img/hidden/Garage2.jpg'
 import { FiArrowRight } from "react-icons/fi";
 import { FiArrowLeft } from "react-icons/fi";
 
@@ -26,42 +26,28 @@ import { FiArrowLeft } from "react-icons/fi";
 
 function Slide() {
 
+    const [imgsl, setimgsl] = useState(0)
+    const slide = [im1, im2, im3, im4, im5, im6, im7, im8, im9, im10, im11, im12, im13, im14, im15, im16, im17]
+    function next() {
+        if (imgsl >= slide.length - 1) {
+            setimgsl(0)
+        }
+        else {
+            setimgsl(imgsl + 1)
+        }
+        console.log(imgsl)
+    }
+    function prev() {
+        if (imgsl <= 0) {
+            setimgsl(slide.length - 1)
+        }
+        else {
+            setimgsl(imgsl - 1)
+        }
 
-    useEffect(() => {
-        const images = document.querySelectorAll(".carousel-image");
-        const prevBtn = document.querySelector("#prevBtn");
-        const nextBtn = document.querySelector("#nextBtn");
+        console.log(imgsl)
+    }
 
-        const nextSlide = () => {
-            const currentSlide = document.querySelector(".current");
-            currentSlide.classList.remove("current");
-            if (currentSlide.nextElementSibling) {
-                currentSlide.nextElementSibling.classList.add("current");
-            } else {
-                images[0].classList.add("current");
-            }
-
-        };
-
-        const prevSlide = () => {
-            const currentSlide = document.querySelector(".current");
-            currentSlide.classList.remove("current");
-            if (currentSlide.previousElementSibling) {
-                currentSlide.previousElementSibling.classList.add("current");
-            } else {
-                images[images.length - 1].classList.add("current");
-            }
-           
-        };
-
-        
-        nextBtn.addEventListener("click", k => {
-            nextSlide();
-        });
-        prevBtn.addEventListener("click", k => {
-            prevSlide();
-        });
-    }, []);
 
 
     return (
@@ -80,29 +66,13 @@ function Slide() {
                 <div className="main-carousel">
                     <div className="carousel">
                         <div className="items">
-                            <img src={im1} alt="" className="carousel-image current" />
-                            <img src={im2} alt="" className="carousel-image" />
-                            <img src={im3} alt="" className="carousel-image" />
-                            <img src={im4} alt="" className="carousel-image" />
-                            <img src={im5} alt="" className="carousel-image"/>
-                            <img src={im6} alt="" className="carousel-image" />
-                            <img src={im7} alt="" className="carousel-image" />
-                            <img src={im8} alt="" className="carousel-image" />
-                            <img src={im9} alt="" className="carousel-image"/>
-                            <img src={im10} alt="" className="carousel-image" />                
-                            <img src={im11} alt="" className="carousel-image" />
-                            <img src={im12} alt="" className="carousel-image" />
-                            <img src={im13} alt="" className="carousel-image" />
-                            <img src={im14} alt="" className="carousel-image"/>
-                            <img src={im15} alt="" className="carousel-image" />
-                            <img src={im16} alt="" className="carousel-image" />
-                            <img src={im17} alt="" className="carousel-image" />
+                            <img src={slide[imgsl]} alt="" className="carousel-image" />
                         </div>
                     </div>
 
                     <div className="buttons">
-                        <FiArrowLeft id="nextBtn" size="3vw"/>
-                        <FiArrowRight id="prevBtn" size="3vw"/>
+                        <FiArrowLeft onClick={prev} id="prevBtn" size="3vw" />
+                        <FiArrowRight onClick={next} id="nextBtn" size="3vw" />
                     </div>
 
                 </div>
