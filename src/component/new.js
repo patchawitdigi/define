ab50f5f16f9dc10bc9a7ab135bb8508f0ac3d47f
Patchawit Muensoontorn <patchawit@digitiveworks.com>
2022-08-26 14:14:35 +0700

    Update new.js

--- src/component/new.js
+++ src/component/new.js
@@ -1,17 +1,22 @@
 import './new.css';
 import delogo from '../img/pointint/logo-menu.png'
 import { useState, useEffect } from 'react';
+import {
+    useParams
+  } from "react-router-dom";
 
 export default function New() {
 
-    // const [Newinfo, setNewinfo] = useState([]);
-    // useEffect(() => {
-    //     fetch(`http://localhost:8202/publicServices/news/list.php?route=${newsId}`).then(
-    //         response => response.json().then(data => {
-    //             setNewinfo(data);
-    //         })
-    //     )
-    // }, [])
+    let { newsId } = useParams();
+    
+    const [Newinfo, setNewinfo] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:8202/publicServices/news/list.php?route=${newsId}`).then(
+            response => response.json().then(data => {
+                setNewinfo(data);
+            })
+        )
+    }, [])
 
     return (
         <div>
@@ -20,6 +25,11 @@ export default function New() {
                 <h3 className="headnew" data-aos="fade-right" >NEWS & MEDIA</h3>
                 <h3 className="headnew2" data-aos="fade-right" data-aos-delay="250"></h3>
             </div>
+            {Newinfo.map(image => {
+                            return (
+                                <div></div>
+                            );
+                        })}
             <p>date</p>
             <p>title</p>
             <img />
